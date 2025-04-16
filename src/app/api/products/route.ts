ab740ae5f5a6301e619fbac130fa33e79dd726ab
saikumar0262 <ksaikumar0262@gmail.com>
2025-04-16 20:08:30 +0530

    updated

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -12,8 +12,7 @@ export async function GET() {
         status: 200,
         headers: {
           "Access-Control-Allow-Origin": "*", 
-          "Access-Control-Allow-Methods": "GET, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type",
+          
         },
       }
     );
